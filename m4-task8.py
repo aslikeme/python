43ee5e46dce1a07a517f5a047ba5fb0f83ffdcf3
Denis Popov <aslikeme@mail.ru>
2023-12-31 14:20:45 +0300

    m4 task8 modify

--- m4-task8.py
+++ m4-task8.py
@@ -3,13 +3,10 @@ number_1 = int(input('Введите первое число: '))
 number_2 = int(input('Введите второе число: '))
 number_3 = int(input('Введите третье число: '))
 
-if number_1 > number_2:
-  if number_1 > number_3:
+if number_1 > number_2 and number_3:
     print(number_1)
-  else:
-    print(number_3)
 else:
-  if number_2 > number_3:
-    print(number_2)
-  else:
-    print(number_3)
+    if number_2 > number_3:
+      print(number_2)
+    else:
+      print(number_3)
