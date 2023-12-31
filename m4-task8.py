fc8a2d562e29a92d969cf83da0c737913b757022
Denis Popov <aslikeme@mail.ru>
2023-12-31 14:31:29 +0300

    module 4 task 8 modify 2

--- m4-task8.py
+++ m4-task8.py
@@ -1,7 +1,5 @@
 
-number_1 = int(input('Введите первое число: '))
-number_2 = int(input('Введите второе число: '))
-number_3 = int(input('Введите третье число: '))
+number_1, number_2, number_3 = int(input('Введите первое число: ')), int(input('Введите второе число: ')), int(input('Введите третье число: '))
 
 if number_1 > number_2 and number_3:
     print(number_1)
